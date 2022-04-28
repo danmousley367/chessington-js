75e28a87a6dd259d87f84be99ac2b21c1a13758a
danmousley367 <dan.mousley@softwire.com>
2022-04-28 15:53:03 +0100

    Pawn can move 2 spaces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,10 +8,36 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
+        const availableMoves = []
         const currentSquare = board.findPiece(this)
-        const newCol = currentSquare.col
-        const newRow = (this.player === Player.WHITE) ? currentSquare.row + 1 : currentSquare.row -1
+        const [col, row] = [currentSquare.col, currentSquare.row]
+        // const newCol = currentSquare.col
+        // const newRow = (this.player === Player.WHITE) ? row + 1 : currentSquare.row -1
+        //
+        // availableMoves.push(Square.at(newRow, newCol))
 
-        return [Square.at(newRow, newCol)];
+        if (this.player === Player.WHITE) {
+            availableMoves.push(Square.at(row + 1, col))
+
+            if (row === 1) {
+                availableMoves.push(Square.at(row + 2, col))
+            }
+        }
+
+        if (this.player === Player.BLACK) {
+            availableMoves.push(Square.at(row - 1, col))
+
+            if (row === 6) {
+                availableMoves.push(Square.at(row - 2, col))
+            }
+        }
+
+        // if (this.player === Player.WHITE && row === 1) {
+        //     availableMoves.push(Square.at(row + 2, col))
+        // }
+        // if (this.player === Player.BLACK && row === 6) {
+        //     availableMoves.push(Square.at(row - 2, col))
+        // }
+        return availableMoves
     }
 }
