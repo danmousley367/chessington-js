3e5ea2a15fa7d91eb3f81992ce4e5dad7555c05b
danmousley367 <dan.mousley@softwire.com>
2022-04-29 09:56:57 +0100

    Knight can move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,27 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const availableMoves = []
+        const currentSquare = board.findPiece(this)
+        const [col, row] = [currentSquare.col, currentSquare.row]
+
+        const getMove = (changeRow, changeCol) => {
+            const rowCondition = changeRow > 0 ? (row + changeRow) < 8 : (row + changeRow) >= 0
+            const colCondition = changeCol > 0 ? (row + changeCol) < 8 : (row + changeCol) >= 0
+            if (rowCondition && colCondition) {
+                availableMoves.push(Square.at(row+changeRow, col+changeCol))
+            }
+        }
+
+        getMove(2, 1)
+        getMove(2, -1)
+        getMove(-2, 1)
+        getMove(-2, -1)
+        getMove(1, 2)
+        getMove(-1, 2)
+        getMove(1, -2)
+        getMove(-1, -2)
+
+        return availableMoves
     }
 }
