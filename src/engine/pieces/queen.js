8ad5c498048ef543cc21e1833ed0642388620dea
danmousley367 <dan.mousley@softwire.com>
2022-04-29 15:32:21 +0100

    Queen cannot move through other pieces

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -10,34 +10,118 @@ export default class Queen extends Piece {
         const availableMoves = []
         const currentSquare = board.findPiece(this)
         const [col, row] = [currentSquare.col, currentSquare.row]
+
+        const checkHasPieceForwardRight = (a) => {
+            for (let x = 1; x < a; x++) {
+                if (board.getPiece(Square.at(row + x, col + x)) !== undefined) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        const checkHasPieceForwardLeft = (a) => {
+            for (let x = 1; x < a; x++) {
+                if (board.getPiece(Square.at(row+x, col-x)) !== undefined) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        const checkHasPieceBackLeft = (a) => {
+            for (let x = 1; x < a; x++) {
+                if (board.getPiece(Square.at(row-x, col-x)) !== undefined) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        const checkHasPieceBackRight = (a) => {
+            for (let x = 1; x < a; x++) {
+                if (board.getPiece(Square.at(row-x, col+x)) !== undefined) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        const checkHasPieceY = (i) => {
+            if (col > i) {
+                for (let j = i+1; j < col; j++) {
+                    if (board.getPiece(Square.at(row, j)) !== undefined) {
+                        return true
+                    }
+                }
+                return false
+            }
+            if (col < i) {
+                for (let j = col+1; j < i; j++) {
+                    if (board.getPiece(Square.at(row, j)) !== undefined) {
+                        return true
+                    }
+                }
+                return false
+            }
+
+        }
+        const checkHasPieceX = (i) => {
+            if (row > i) {
+                for (let j = i+1; j < row; j++) {
+                    if (board.getPiece(Square.at(j, col)) !== undefined) {
+                        return true
+                    }
+                }
+                return false
+            }
+            if (row < i) {
+                for (let j = row+1; j < i; j++) {
+                    if (board.getPiece(Square.at(j, col)) !== undefined) {
+                        return true
+                    }
+                }
+                return false
+            }
+
+        }
+
         let i = 1
         let j = 1
         let k = 1
         let l = 1
 
         while (col+i < 8 && row+i < 8) {
-            availableMoves.push(Square.at(row+i, col+i))
+            if (!checkHasPieceForwardRight(i)) {
+                availableMoves.push(Square.at(row + i, col + i))
+            }
             i++
         }
         while (col-k >= 0 && row+k < 8) {
-            availableMoves.push(Square.at(row+k, col-k))
+            if (!checkHasPieceForwardLeft(k)) {
+                availableMoves.push(Square.at(row + k, col - k))
+            }
             k++
         }
         while (col-j >= 0 && row-j >= 0) {
-            availableMoves.push(Square.at(row-j, col-j))
+            if (!checkHasPieceBackLeft(j)) {
+                availableMoves.push(Square.at(row - j, col - j))
+            }
             j++
         }
         while (col+l < 8 && row-l >= 0) {
-            availableMoves.push(Square.at(row-l, col+l))
+            if (!checkHasPieceBackRight(l)) {
+                availableMoves.push(Square.at(row - l, col + l))
+            }
             l++
         }
 
         for (let m = 0; m < 8; m++) {
             if (m !== col) {
-                availableMoves.push(Square.at(row, m))
+                if (!checkHasPieceY(m)) {availableMoves.push(Square.at(row, m))}
             }
             if (m !== row) {
-                availableMoves.push(Square.at(m, col))
+                if (!checkHasPieceX(m)) {availableMoves.push(Square.at(m, col))}
             }
         }
 
